00a7c9cb649d74d5793524de7c84c57bb3108350
Tadashi Izuno <tadashi.izuno@gmail.com>
2017-01-09 14:11:23 +0900

    add basic files

---
+++ src/main/java/com/example/springboot/controller/GreetingController.java
@@ -0,0 +1,40 @@
+package com.example.springboot.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.bean.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+	private static final String template = "Hello, %s!";
+
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@Profile(value = "foo")
+	@RequestMapping("/foo/greeting")
+	public Greeting fooGreeting(@RequestParam(value = "name", defaultValue = "Foo World") String name) {
+		logger.debug(activeProfile);
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@Profile(value = "bar")
+	@RequestMapping("/bar/greeting")
+	public Greeting barGreeting(@RequestParam(value = "name", defaultValue = "Bar World") String name) {
+		logger.debug(activeProfile);
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
